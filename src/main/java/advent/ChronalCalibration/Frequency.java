ccc434f716c7775c8a74c1359ea64ffe21890889
RichStone <richard.had@hotmail.de>
2019-07-25 18:51:16 +0200

    solve part 1

--- src/main/java/advent/ChronalCalibration/Frequency.java
+++ src/main/java/advent/ChronalCalibration/Frequency.java
@@ -1,5 +1,11 @@
 package advent.ChronalCalibration;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class Frequency {
 
     private int current;
@@ -14,5 +20,23 @@ public class Frequency {
 
     public void changeBy(int value) {
         current += value;
+    }
+
+    public int getFrequencyChangeFrom(InputStream frequencyHistory) {
+        Scanner s = new Scanner(frequencyHistory);
+        int frequencyChange = 0;
+
+        while(s.hasNextInt()) {
+            frequencyChange += s.nextInt();
+        }
+        return frequencyChange;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Frequency frequency = new Frequency();
+        File inputFile = new File("/Users/rich/code/src/github.com/AoC-2018/src/main/java/advent/ChronalCalibration/input.txt");
+        InputStream input = new FileInputStream(inputFile);
+
+        System.out.println(frequency.getFrequencyChangeFrom(input));
     }
 }
--- src/test/java/advent/FrequencyTest.java
+++ src/test/java/advent/FrequencyTest.java
@@ -2,6 +2,10 @@ package advent;
 
 import advent.ChronalCalibration.Frequency;
 import org.junit.Test;
+
+import java.io.*;
+import java.util.Scanner;
+
 import static org.junit.Assert.*;
 
 public class FrequencyTest {
@@ -9,25 +13,36 @@ public class FrequencyTest {
     public void initialSumShouldBeZero() {
         Frequency frequency = new Frequency();
         int currentFrequency = frequency.getCurrent();
-        assertEquals(currentFrequency, 0);
+        assertEquals(0, currentFrequency);
     }
 
     @Test
     public void newFrequencyShouldBeOneAfterChangingFrequencyByPlusOne() {
         Frequency frequency = new Frequency();
         frequency.changeBy(1);
-        assertEquals(frequency.getCurrent(), 1);
+        assertEquals(1, frequency.getCurrent());
     }
 
     @Test
     public void newFrequencyShouldBeMinusTwoAfterChangingByMinusTwo() {
         Frequency frequency = new Frequency();
         frequency.changeBy(-2);
-        assertEquals(frequency.getCurrent(), -2);
+        assertEquals(-2, frequency.getCurrent());
     }
 
     @Test
-    public void frequencyShouldBe3After3ChangesByOne() {
+    public void shouldGetCorrectFrequencyChangeFromTestInputStream() {
+        Frequency frequency = new Frequency();
+        InputStream testInput = new ByteArrayInputStream("+1\n+1\n+1\n".getBytes());
+        int frequencyChange = frequency.getFrequencyChangeFrom(testInput);
+        assertEquals(3, frequencyChange);
 
+        testInput = new ByteArrayInputStream("+1\n+1\n-2\n".getBytes());
+        frequencyChange = frequency.getFrequencyChangeFrom(testInput);
+        assertEquals(0, frequencyChange);
+
+        testInput = new ByteArrayInputStream("-1\n-2\n-3\n".getBytes());
+        frequencyChange = frequency.getFrequencyChangeFrom(testInput);
+        assertEquals(-6, frequencyChange);
     }
 }

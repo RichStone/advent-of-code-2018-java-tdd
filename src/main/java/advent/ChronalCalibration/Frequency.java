d9fa2435e375a54b91a73cc954682f6f81532f96
RichStone <richard.had@hotmail.de>
2019-08-16 17:42:59 +0200

    solve part 2

--- src/main/java/advent/ChronalCalibration/Frequency.java
+++ src/main/java/advent/ChronalCalibration/Frequency.java
@@ -4,39 +4,102 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.util.Scanner;
+import java.net.URL;
+import java.util.*;
 
 public class Frequency {
 
-    private int current;
+    private int currentFrequencySum;
+    private Set<Integer> sums;
+    public List<Integer> frequencyHistory;
+    private ListIterator<Integer> listIterator;
 
     public Frequency() {
-        this.current = 0;
+        currentFrequencySum = 0;
+        sums = new HashSet<>();
+        sums.add(0);
     }
 
-    public int getCurrent() {
-        return current;
+    public Frequency(InputStream frequencyHistory) {
+        currentFrequencySum = 0;
+        sums = new HashSet<>();
+        sums.add(0);
+
+        this.frequencyHistory = initializeFrequencyHistory(frequencyHistory);
+        listIterator = this.frequencyHistory.listIterator(0);
     }
 
-    public void changeBy(int value) {
-        current += value;
+    private List<Integer> initializeFrequencyHistory(InputStream frequencyHistory) {
+        Scanner s = new Scanner(frequencyHistory);
+        List<Integer> parsedFrequencyHistory = new LinkedList<>();
+
+        while (s.hasNextInt()) {
+            parsedFrequencyHistory.add(s.nextInt());
+        }
+        return parsedFrequencyHistory;
+    }
+
+    public int getCurrentFrequencySum() {
+        return currentFrequencySum;
+    }
+
+    private void addToFrequencySum(int value) {
+        currentFrequencySum += value;
+    }
+
+    private void addToSumsCache(int currentSum) {
+        sums.add(currentSum);
     }
 
     public int getFrequencyChangeFrom(InputStream frequencyHistory) {
         Scanner s = new Scanner(frequencyHistory);
-        int frequencyChange = 0;
 
         while(s.hasNextInt()) {
-            frequencyChange += s.nextInt();
+            addToFrequencySum(s.nextInt());
+            addToSumsCache(currentFrequencySum);
         }
-        return frequencyChange;
+        return currentFrequencySum;
+    }
+
+    public Integer getRepeatedSum() {
+        while (listIterator.hasNext()) {
+            int nextFrequency = listIterator.next();
+            currentFrequencySum += nextFrequency;
+
+            if (isRepeatedSum(currentFrequencySum)) {
+                return currentFrequencySum;
+            }
+
+            addToSumsCache(currentFrequencySum);
+
+            if (!listIterator.hasNext()) {
+                listIterator = frequencyHistory.listIterator(0);
+            }
+        }
+        return null;
+    }
+
+    private boolean isRepeatedSum(int frequencySum) {
+        return sums.contains(frequencySum);
+    }
+
+    public Set<Integer> getFrequencyChangeSums() {
+        return sums;
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-        Frequency frequency = new Frequency();
-        File inputFile = new File("/Users/rich/code/src/github.com/AoC-2018/src/main/java/advent/ChronalCalibration/input.txt");
-        InputStream input = new FileInputStream(inputFile);
+        URL url = ClassLoader.getSystemClassLoader().getResource("inputDay1.txt");
+        assert url != null;
+        File inputFile = new File(url.getFile());
 
+        // Solution of 1st part
+        Frequency frequency = new Frequency();
+        InputStream input = new FileInputStream(inputFile);
         System.out.println(frequency.getFrequencyChangeFrom(input));
+
+        // Solution of 2nd part
+        input = new FileInputStream(inputFile);
+        Frequency frequency2 = new Frequency(input);
+        System.out.println(frequency2.getRepeatedSum());
     }
 }
--- src/test/java/advent/FrequencyTest.java
+++ src/test/java/advent/FrequencyTest.java
@@ -4,7 +4,7 @@ import advent.ChronalCalibration.Frequency;
 import org.junit.Test;
 
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
@@ -12,37 +12,85 @@ public class FrequencyTest {
     @Test
     public void initialSumShouldBeZero() {
         Frequency frequency = new Frequency();
-        int currentFrequency = frequency.getCurrent();
+        int currentFrequency = frequency.getCurrentFrequencySum();
         assertEquals(0, currentFrequency);
     }
 
     @Test
-    public void newFrequencyShouldBeOneAfterChangingFrequencyByPlusOne() {
-        Frequency frequency = new Frequency();
-        frequency.changeBy(1);
-        assertEquals(1, frequency.getCurrent());
-    }
-
-    @Test
-    public void newFrequencyShouldBeMinusTwoAfterChangingByMinusTwo() {
-        Frequency frequency = new Frequency();
-        frequency.changeBy(-2);
-        assertEquals(-2, frequency.getCurrent());
-    }
-
-    @Test
     public void shouldGetCorrectFrequencyChangeFromTestInputStream() {
-        Frequency frequency = new Frequency();
         InputStream testInput = new ByteArrayInputStream("+1\n+1\n+1\n".getBytes());
-        int frequencyChange = frequency.getFrequencyChangeFrom(testInput);
+        int frequencyChange = new Frequency().getFrequencyChangeFrom(testInput);
         assertEquals(3, frequencyChange);
 
         testInput = new ByteArrayInputStream("+1\n+1\n-2\n".getBytes());
-        frequencyChange = frequency.getFrequencyChangeFrom(testInput);
+        frequencyChange = new Frequency().getFrequencyChangeFrom(testInput);
         assertEquals(0, frequencyChange);
 
         testInput = new ByteArrayInputStream("-1\n-2\n-3\n".getBytes());
-        frequencyChange = frequency.getFrequencyChangeFrom(testInput);
+        frequencyChange = new Frequency().getFrequencyChangeFrom(testInput);
         assertEquals(-6, frequencyChange);
+    }
+
+    @Test
+    public void shouldCacheSumOfFrequencies() {
+        Frequency freq = new Frequency();
+        InputStream testInput = new ByteArrayInputStream("+1\n+1\n+1\n".getBytes());
+        int frequencyChange = freq.getFrequencyChangeFrom(testInput);
+        Set<Integer> sums = freq.getFrequencyChangeSums();
+        assertTrue(sums.contains(0));
+        assertTrue(sums.contains(1));
+        assertTrue(sums.contains(2));
+        assertTrue(sums.contains(3));
+    }
+
+    @Test
+    public void shouldGetTheFirstRepeatingFrequencySum() {
+        InputStream testInput = new ByteArrayInputStream("+3\n+3\n+4\n-2\n-4\n".getBytes());
+        Frequency freq = new Frequency(testInput);
+        int repeatedSum = freq.getRepeatedSum();
+        assertEquals(10, repeatedSum);
+
+        testInput = new ByteArrayInputStream("-6\n+3\n+8\n+5\n-6\n".getBytes());
+        freq = new Frequency(testInput);
+        repeatedSum = freq.getRepeatedSum();
+        assertEquals(5, repeatedSum);
+
+        testInput = new ByteArrayInputStream("+7\n+7\n-2\n-7\n-4\n".getBytes());
+        freq = new Frequency(testInput);
+        repeatedSum = freq.getRepeatedSum();
+        assertEquals(14, repeatedSum);
+    }
+
+    @Test
+    public void shouldGetTheFirstRepeatingFrequencySumWhenZero() {
+        InputStream testInput = new ByteArrayInputStream("+1\n-1\n-2\n".getBytes());
+        Frequency freq = new Frequency(testInput);
+        int repeatedSum = freq.getRepeatedSum();
+        assertEquals(0, repeatedSum);
+    }
+
+    @Test
+    public void shouldLoopUntilRepeatedSumIsFound() {
+        InputStream testInput = new ByteArrayInputStream("+1\n-2\n+3\n+1\n".getBytes());
+        Frequency freq = new Frequency(testInput);
+        int repeatedSum = freq.getRepeatedSum();
+        assertEquals(2, repeatedSum);
+    }
+
+    @Test
+    public void shouldInitializeFrequencyHistoryCorrectly() {
+        InputStream testInput = new ByteArrayInputStream("+1\n-1\n-2\n".getBytes());
+        Frequency freq = new Frequency(testInput);
+        List<Integer> frequencyHistory = freq.frequencyHistory;
+
+        int frequencyHistoryLength = frequencyHistory.size();
+        assertEquals(3, frequencyHistoryLength);
+
+        int freq1 = frequencyHistory.get(0);
+        int freq2 = frequencyHistory.get(1);
+        int freq3 = frequencyHistory.get(2);
+        assertEquals(1, freq1);
+        assertEquals(-1, freq2);
+        assertEquals(-2, freq3);
     }
 }
